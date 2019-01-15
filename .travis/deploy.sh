cc6b62f4d3ca4f5d546dc51abee5950f6d4d11c2
Xavier Burrow <jxburrow@gmail.com>
2019-01-15 18:33:26 +1100

    disable build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,8 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+git status
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git remote
+git branch
 git push deploy
