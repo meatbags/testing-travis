afab77e5b5f0c156a5e9e9d0bcb59291528250e3
Xavier Burrow <jxburrow@gmail.com>
2019-01-15 18:29:44 +1100

    disable build

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,4 +6,4 @@ ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-git push deploy master
+git push deploy
