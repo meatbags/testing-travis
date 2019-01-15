eab1514f3b8696466a9d9d3859e6555f6c632cf3
Xavier Burrow <jxburrow@gmail.com>
2019-01-15 16:26:03 +1100

    change ssh dir

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 id_rsa # Allow read access to the private key
-ssh-add id_rsa # Add the private key to SSH
+chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
+ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
